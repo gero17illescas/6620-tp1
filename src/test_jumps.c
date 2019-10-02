44f92d2da09d918e7c2d3db12f26d0adc7eb5f4f
Milagros Cruz <mivircruz@gmail.com>
2019-10-02 01:57:48 -0300

    algunas mejoras pero sigue sin compilar

--- src/test_jumps.c
+++ src/test_jumps.c
@@ -28,41 +28,42 @@ int main() {
 
     ant_t ant;
     ant_t* a = &ant;
+    ant_t* ant_result;
 
     a->x = 1;
     a->y = 1;
     a->o = 0;
-    ant_t* ant_result = move_forward(a, 3, 3);
+   ant_result = move_forward(a, 3, 3);
     printf("test_8: the result %i  should be: 0 \n", ant_result->y);
 
     a->x = 1;
     a->y = 1;
     a->o = 1;
-    ant_t* ant_result = move_forward(a, 3, 3);
+    ant_result = move_forward(a, 3, 3);
     printf("test_9: the result %i  should be: 2 \n", ant_result->y);
 
     a->x = 1;
     a->y = 1;
     a->o = 2;
-    ant_t* ant_result = move_forward(a, 3, 3);
+    ant_result = move_forward(a, 3, 3);
     printf("test_10: the result %i  should be: 0 \n", ant_result->x);
 
     a->x = 1;
     a->y = 1;
     a->o = 3;
-    ant_t* ant_result = move_forward(a, 3, 3);
+    ant_result = move_forward(a, 3, 3);
     printf("test_11: the result %i  should be: 2 \n", ant_result->x);
 
     a->x = 0;
     a->y = 0;
     a->o = 0;
-    ant_t* ant_result = move_forward(a, 3, 3);
+    ant_result = move_forward(a, 3, 3);
     printf("test_12: the result %i  should be: 2 \n", ant_result->y);
 
     a->x = 3;
     a->y = 3;
     a->o = 1;
-    ant_t* ant_result = move_forward(a, 3, 3);
+    ant_result = move_forward(a, 3, 3);
     printf("test_13: the result %i  should be: 1\n", ant_result->y);
 
     a->x = 3;
@@ -74,7 +75,7 @@ int main() {
     a->x = 0;
     a->y = 0;
     a->o = 3;
-    ant_t* ant_result = move_forward(a, 3, 3);
+    ant_result = move_forward(a, 3, 3);
     printf("test_1: the result %i  should be: 2 \n", ant_result->x);
 
 
