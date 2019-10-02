44088f03518b890f0bfe247a011493c4d9c03b13
gero17illescas <gero17illescas@gmail.com>
2019-10-02 19:48:55 -0300

    Cambios en las pruebas

--- src/test_jumps.c
+++ src/test_jumps.c
@@ -46,37 +46,37 @@ int main() {
     a->y = 1;
     a->o = 2;
     ant_result = move_forward(a, 3, 3);
-    printf("test_10: the result %i  should be: 0 \n", ant_result->x);
+    printf("test_10: the result %i  should be: 2 \n", ant_result->x);
 
     a->x = 1;
     a->y = 1;
     a->o = 3;
     ant_result = move_forward(a, 3, 3);
-    printf("test_11: the result %i  should be: 2 \n", ant_result->x);
+    printf("test_11: the result %i  should be: 0 \n", ant_result->x);
 
     a->x = 0;
     a->y = 0;
     a->o = 0;
     ant_result = move_forward(a, 3, 3);
-    printf("test_12: the result %i  should be: 2 \n", ant_result->y);
+    printf("test_12: the result %u  should be: 2 \n", ant_result->y);
 
-    a->x = 3;
-    a->y = 3;
+    a->x = 2;
+    a->y = 2;
     a->o = 1;
     ant_result = move_forward(a, 3, 3);
-    printf("test_13: the result %i  should be: 1\n", ant_result->y);
+    printf("test_13: the result %u  should be: 1\n", ant_result->y);
 
-    a->x = 3;
-    a->y = 3;
+    a->x = 2;
+    a->y = 2;
     a->o = 2;
     ant_t* ant_result = move_forward(a, 3, 3);
-    printf("test_14: the result %i  should be: 1 \n", ant_result->x);
+    printf("test_14: the result %u  should be: 0 \n", ant_result->x);
 
     a->x = 0;
     a->y = 0;
     a->o = 3;
     ant_result = move_forward(a, 3, 3);
-    printf("test_1: the result %i  should be: 2 \n", ant_result->x);
+    printf("test_15: the result %u  should be: 2 \n", ant_result->x);
 
 
     return 0;
